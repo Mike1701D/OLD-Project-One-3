10018a3d32f8469e9aa606ff4ec34d8a486e2c92
BRIAN YACK <bdyack@gmail.com>
2017-09-18 19:56:49 -0500

    adding firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -85,3 +85,70 @@ function onYouTubeIframeAPIReady() {
         }
     });
 }
+
+
+
+// Initialize Firebase
+    var config = {
+    apiKey: "AIzaSyA8QWyDbrikOwlWMnm2nDnIId8zhKrGjE0",
+    authDomain: "project-one-bbbb8.firebaseapp.com",
+    databaseURL: "https://project-one-bbbb8.firebaseio.com",
+    projectId: "project-one-bbbb8",
+    storageBucket: "project-one-bbbb8.appspot.com",
+    messagingSenderId: "678586800756"
+  };
+  
+
+    firebase.initializeApp(config);
+
+    // Create a variable to reference the database.
+    var database = firebase.database();
+
+    // Initial Values
+    var name = "";
+    var email = "";
+    var age = 0;
+    var comment = "";
+
+    // Capture Button Click
+    $("#add-user").on("click", function(event) {
+      event.preventDefault();
+
+      // Grabbed values from text boxes
+      name = $("#name-input").val().trim();
+      email = $("#email-input").val().trim();
+      age = $("#age-input").val().trim();
+      comment = $("#comment-input").val().trim();
+
+      // Code for handling the push
+      database.ref().push({
+        name: name,
+        email: email,
+        age: age,
+        comment: comment,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+      });
+
+    });
+
+    // Firebase watcher + initial loader + order/limit HINT: .on("child_added"
+    database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+      // storing the snapshot.val() in a variable for convenience
+      var sv = snapshot.val();
+
+      // Console.loging the last user's data
+      console.log(sv.name);
+      console.log(sv.email);
+      console.log(sv.age);
+      console.log(sv.comment);
+
+      // Change the HTML to reflect
+      $("#name-display").html(sv.name);
+      $("#email-display").html(sv.email);
+      $("#age-display").html(sv.age);
+      $("#comment-display").html(sv.comment);
+
+      // Handle the errors
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
